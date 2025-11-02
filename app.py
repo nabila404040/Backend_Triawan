f0d789a77c609e43ab7b72f92012696cbd24be68
nabila404040 <tugasbuat545gmail.com>
2025-11-02 14:06:49 +0700

    push aplikasi backend

---
+++ app.py
@@ -0,0 +1,16 @@
+from flask import Flask
+from config.database import engine, Base
+from routes.web import web
+import models.menu_model  # register model
+
+app = Flask(__name__)
+
+# Buat tabel otomatis (kalau belum ada di DB)
+Base.metadata.create_all(bind=engine)
+
+# Daftarkan route
+app.register_blueprint(web)
+
+if __name__ == "__main__":
+    app.run(debug=True, host="0.0.0.0", port=5000)
+
